432e6a34236c861e628d4a83e4d95f97331e4325
Naren Kukreja <narenkukreja@gmail.com>
2015-12-20 19:25:03 -0500

    Initial commit

---
+++ android/src/com/naren/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.naren.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.naren.game.FlappyBot;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlappyBot(), config);
+	}
+}
---
+++ desktop/src/com/naren/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.naren.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.naren.game.FlappyBot;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyBot(), config);
+	}
+}
