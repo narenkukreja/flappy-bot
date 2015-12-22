8f0af80a9011c6ec9678522170678dee552d2f4a
Naren Kukreja <narenkukreja@gmail.com>
2015-12-22 18:00:54 -0500

    Added Background and Play button to the stage

--- core/src/com/naren/game/states/MenuState.java
+++ core/src/com/naren/game/states/MenuState.java
@@ -1,11 +1,18 @@
 package com.naren.game.states;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.naren.game.FlappyBot;
 
 public class MenuState extends State {
 
+    private Texture background, playBtn;
+
     public MenuState(GameStateManager gsm) {
         super(gsm);
+        background = new Texture("bg.png");
+        playBtn = new Texture("playBtn.png");
+
     }
 
     @Override
@@ -20,6 +27,11 @@ public class MenuState extends State {
 
     @Override
     public void render(SpriteBatch sb) {
+
+        sb.begin();
+        sb.draw(background, 0, 0, FlappyBot.WIDTH, FlappyBot.HEIGHT);
+        sb.draw(playBtn, (FlappyBot.WIDTH/2) - (playBtn.getWidth()/2) , FlappyBot.HEIGHT/2);
+        sb.end();
 
     }
 }
